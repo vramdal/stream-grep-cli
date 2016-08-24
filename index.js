b8bda753156583d565bca4889c4c534e9fd9a702
Vidar Ramdal <Vidar.Ramdal@norgesgruppen.no>
2016-08-24 13:31:50 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var readline = require('readline');
+var chalk = require('chalk');
+var program = require('commander');
+
+program
+    .arguments('<regexppattern>', 'Regexp pattern')
+    .option('-f, --flags <flags>', 'Regexp flags', "")
+    .action(function(pattern, cmd) {
+        var regexp = new RegExp(pattern, cmd.flags);
+        var matchedLines = [];
+        var rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            terminal: false
+        });
+
+        rl.on('line', function(line){
+            if (regexp.test(line)) {
+                matchedLines.push(line);
+                console.log(chalk.red(line));
+            } else {
+                console.log(line);
+            }
+        });
+        rl.on('close', function() {
+            if (matchedLines.length > 0) {
+                if (cmd.summarize) {
+                    console.log(chalk.bold.red("Matches " + regexp.toString() + ":"));
+                    for (var i = 0; i < matchedLines.length; i++) {
+                        var line = matchedLines[i];
+                        console.log(chalk.red(line));
+                    }
+                }
+                process.exit(1)
+            } else {
+                console.log(chalk.green("No line matching " + regexp.toString() + " was found"));
+                process.exit(0);
+            }
+        })
+
+    }).parse(process.argv)
+;
